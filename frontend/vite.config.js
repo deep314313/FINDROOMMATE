7744521f0c9c2a15f7a40660f323d54ac89b40ef
deep314313 <123625000+deep314313@users.noreply.github.com>
2025-04-02 22:17:10 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,15 @@
-{
-  "rewrites": [
-    { "source": "/(.*)", "destination": "/" }
-  ]
-}
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist', // Default output directory
+    // Other build options...
+  },
+  base: '/', // Default base URL
+  server: {
+    // Development server options (won't affect Vercel production)
+  }
+});
