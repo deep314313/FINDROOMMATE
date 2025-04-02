21f9ba546e9e720f887da8d93fb279d4067c97bc
deep314313 <d.gupta9868@gmail.com>
2025-04-02 22:19:52 +0530

    done

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,25 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Default output directory
-    // Other build options...
-  },
-  base: '/', // Default base URL
   server: {
-    // Development server options (won't affect Vercel production)
-  }
-});
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: process.env.VITE_API_URL,
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  esbuild: {
+    loader: 'jsx',
+    include: /\.jsx?$/,
+    exclude: [],
+  },
+})
