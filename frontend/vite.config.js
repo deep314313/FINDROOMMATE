826419b616230d30f3db97b880fe5ecec95e7e94
deep314313 <d.gupta9868@gmail.com>
2025-04-02 22:08:37 +0530

    vite chnaged

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: process.env.VITE_API_URL,
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  esbuild: {
+    loader: 'jsx',
+    include: /\.jsx?$/,
+    exclude: [],
+  },
 })
