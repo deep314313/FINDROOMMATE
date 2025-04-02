f49ecc69a7d2e09881522425d144896093472a40
deep314313 <123625000+deep314313@users.noreply.github.com>
2025-04-02 22:11:42 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,25 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: process.env.VITE_API_URL,
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  esbuild: {
-    loader: 'jsx',
-    include: /\.jsx?$/,
-    exclude: [],
-  },
-})
+{
+  "rewrites": [
+    { "source": "/(.*)", "destination": "/" }
+  ]
+}
